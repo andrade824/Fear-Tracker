566d5a6b0075ff6682f32f0c09d7c6611bb13c61
Jerome Selig <jerome.a.selig@gmail.com>
2014-11-20 16:30:18 -0800

    Restructuring test project to include UART for debugging purposes. ADC is not completely functional.

--- Test Code/ADCTest/ADCTest/src/main.c
+++ Test Code/ADCTest/ADCTest/src/main.c
@@ -9,28 +9,43 @@
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
+#include "../inc/config.h"
+#include "../inc/UART.h"
+
 void InitADC(void);
 
 int main(void)
 {
-	
+	const char const * string = "The UART Works!\n\r";
+	UARTInit(UBRR);
+	int length = strlen(string);
+
+	while(1)
+	{
+		for(int i = 0; i < length; i++)
+		UARTTransmit(string[i]);
+	}
+
 	PORTB &= ~(_BV(PORTB5));	// Clear PB5, so it drives low by default (need to do this before switching it to an output)
 	DDRB |= _BV(PORTB5);		// Set PB5 as an output
 	
 	InitADC();
 	
     while(1)
-   {	  
-       if( ADCH // somthing something)
+   {	 
+	   while(ADSC);	//wait for conversion to finish
+	   
+       if( ADCH >= 1 )	// if there is a converted voltage
 		{
 			PORTB |= (1<<PORTB5);
-			_delay_ms(500);			// Delay half a second
+			//_delay_ms(500);			// Delay half a second
 		}
 		else
 		{
 			PORTB &= ~(1<<PORTB5);						
 		
-			_delay_ms(500);			// Delay half a second		
+			//_delay_ms(500);			// Delay half a second		
 		}
 		
    }
@@ -44,9 +59,13 @@ void InitADC(void)
 	ADMUX |= (1<<REFS0) | (1<<REFS1); //internal 1.1v voltage reference  
 	
 	ADMUX |= (1 << ADLAR); // Left adjust ADC
-	ADMUX |= (1 << MUX0); //a1 is input
+	ADMUX |= (1 << MUX2); //a2 is input
+	
+	DIDR0 |= _BV(ADC2D); //disabling digital input for pin A2
 	
  	ADCSRA |= _BV(ADEN);//enable ADC
+	PRR &= ~(_BV(PRADC)); // Disable the power reduction ADC bit
 	ADCSRA |= _BV(ADSC); // start conversion, use this when ADC is in free running mode to start conversion
 	//otherwise it is triggered by something
 }
+
