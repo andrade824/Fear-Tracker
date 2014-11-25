01c054f26f9806aa22d9ae17627dba2f36ffc0d6
Devon Andrade <andrade824@gmail.com>
2014-11-25 02:47:30 -0800

    Fixed ADC test code

--- Test Code/ADCTest/ADCTest/src/main.c
+++ Test Code/ADCTest/ADCTest/src/main.c
@@ -17,51 +17,33 @@ void InitADC(void);
 
 int main(void)
 {
-	const char const * string = "The UART Works!\n\r";
 	UARTInit(UBRR);
-	int length = strlen(string);
-
-	while(1)
-	{
-		for(int i = 0; i < length; i++)
-		UARTTransmit(string[i]);
-	}
+	InitADC();
 
 	PORTB &= ~(_BV(PORTB5));	// Clear PB5, so it drives low by default (need to do this before switching it to an output)
 	DDRB |= _BV(PORTB5);		// Set PB5 as an output
-	
-	InitADC();
-	
+
     while(1)
    {	 
-	   while(ADSC);	//wait for conversion to finish
+	   while(ADCSRA & _BV(ADSC));	//wait for conversion to finish
 	   
-       if( ADCH >= 1 )	// if there is a converted voltage
-		{
-			PORTB |= (1<<PORTB5);
-			//_delay_ms(500);			// Delay half a second
-		}
-		else
-		{
-			PORTB &= ~(1<<PORTB5);						
-		
-			//_delay_ms(500);			// Delay half a second		
-		}
-		
+	   UARTTransmit(ADCH);
+	   _delay_ms(10);
+	   ADCSRA |= _BV(ADSC);
    }
 	return 0;
 }
 
 void InitADC(void)
 {
-	ADCSRA |= (1<<ADPS0) | (1 << ADPS1) | (1<<ADPS2); // set ADC sample rate to 128 - 62.5kHz @ 8Mhz
+	ADCSRA |= _BV(ADPS1) | _BV(ADPS2); // set ADC sample rate to - 125kHz @ 8Mhz (div 64 prescaler)
 	
-	ADMUX |= (1<<REFS0) | (1<<REFS1); //internal 1.1v voltage reference  
+	ADMUX |= _BV(REFS0) | _BV(REFS1); //internal 1.1v voltage reference  
 	
-	ADMUX |= (1 << ADLAR); // Left adjust ADC
-	ADMUX |= (1 << MUX2); //a2 is input
+	ADMUX |= _BV(ADLAR); // Left adjust ADC
+	ADMUX |= _BV(MUX0); // a1 is input
 	
-	DIDR0 |= _BV(ADC2D); //disabling digital input for pin A2
+	DIDR0 |= _BV(ADC1D); //disabling digital input for pin A2
 	
  	ADCSRA |= _BV(ADEN);//enable ADC
 	PRR &= ~(_BV(PRADC)); // Disable the power reduction ADC bit
